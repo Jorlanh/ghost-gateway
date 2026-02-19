4082196e9351c5ee6cfa4fcd5782ca83d9182b5c
Jorlan Heider <jorlan25.js@gmail.com>
2026-02-19 12:34:14 -0300

    atualização

---
+++ ghost-gateway/src/test/java/com/ghost/ghost_gateway/GhostGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.ghost_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
